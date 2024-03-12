f3129e13706a15a55bef048af38497c7b5aa70cd
alexanderhoyskel <1664AlfredAnna>
2024-03-12 18:11:50 +0100

    Move files to right place

--- src/main/java/com/example/oblig/ObligApplication.java
+++ src/main/java/com/example/oblig/ObligApplication.java
@@ -2,6 +2,11 @@ package com.example.oblig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ObligApplication {
@@ -10,4 +15,23 @@ public class ObligApplication {
         SpringApplication.run(ObligApplication.class, args);
     }
 
+    @RestController
+    public static class TicketController {
+        private final ArrayList<Ticket> allTickets = new ArrayList<>();
+        @PostMapping("/PostTickets")
+        public void saveTickets (Ticket inTicket){
+            allTickets.add(inTicket);
+        }
+
+        @GetMapping("/GetTickets")
+        public ArrayList<Ticket> getAllTickets(){
+            return allTickets;
+        }
+
+        @GetMapping("/DeleteAll")
+        public void deleteAll(){
+            allTickets.clear();
+        }
+
+    }
 }
