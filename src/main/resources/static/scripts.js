ff3b5120a430d00ef1d083ba674379db24e18ee1
alexanderhoyskel <1664AlfredAnna>
2024-03-12 18:05:36 +0100

    Add server functionalities

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,49 +1,47 @@
-let kinobilleter = [];
-
 function visError(id, message) {
-    const element = document.getElementById(id);
-    element.innerHTML = message;
-    element.style.color = "red";
+    $("#" + id).html(message).css("color", "red");
 }
 
 function kjopBillett(){
-    const film = document.getElementById("film");
-    const antall = document.getElementById("antallBilleter");
-    const fornavn = document.getElementById("fornavn");
-    const etternavn = document.getElementById("etternavn");
-    const telefonnr = document.getElementById("telefonnr");
-    const epost = document.getElementById("epost");
+    const film = $("#film").val();
+    let antall = $("#antallBilleter").val();
+    const fornavn = $("#fornavn").val();
+    const etternavn = $("#etternavn").val();
+    const telefonNr = $("#telefonnr").val();
+    const epost = $("#epost").val();
 
     let error = false;
-    if(film.value === ""){
+    if(film === ""){
         visError("errorFilm", "Ikke valgt film")
         error = true;
     }
 
-    const antallTest = Number(antall.value)
+    const antallTest = Number(antall)
     if(isNaN(antallTest) || antallTest < 1 ) {
         visError("errorAntall", "Ikke tastet inn riktig")
         error = true;
+    }else{
+        antall = Number(antall)
     }
 
-    if( fornavn.value === "" ) {
+    if( fornavn === "" ) {
         visError("errorFornavn", "Ikke tastet inn fornavn")
         error = true;
     }
 
-    if( etternavn.value === "" ) {
+    if( etternavn === "" ) {
         visError("errorEtternavn", "Ikke tastet inn etternavn")
         error = true;
     }
 
-    const resultTelefonnr = /^\d+$/.test(telefonnr.value);
+    const resultTelefonnr = /^\d+$/.test(telefonNr);
     if (!resultTelefonnr) {
         visError("errorTelefonnr", "Ikke tastet inn gyldig telefonnummer (kun tall)");
         error = true;
     }
 
-    const resultEpost = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(epost.value);
-    if (epost.value === "" || !resultEpost) {
+    const resultEpost = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(epost);
+    if (epost === "" || !resultEpost) {
         visError("errorEpost", "Ikke tastet inn gyldig epostadresse");
         error = true;
     }
@@ -53,49 +51,59 @@ function kjopBillett(){
     }
 
     const nyBillet = {
-        film: film.value,
-        antall: antall.value,
-        fornavn: fornavn.value,
-        etternavn: etternavn.value,
-        telefonnr: telefonnr.value,
-        epost: epost.value
+        film: film,
+        antall: antall,
+        fornavn: fornavn,
+        etternavn: etternavn,
+        telefonNr: telefonNr,
+        epost: epost
     };
 
-    kinobilleter.push(nyBillet);
+    $.post("http://localhost:8080/PostTickets", nyBillet, function (){
+        hentBilletter();
+    })
 
-    visBilleter();
 
-    film.value = "";
-    antall.value = "";
-    fornavn.value = "";
-    etternavn.value = "";
-    telefonnr.value = "";
-    epost.value = "";
-    document.getElementById("errorAntall").innerHTML="";
-    document.getElementById("errorFilm").innerHTML="";
-    document.getElementById("errorFornavn").innerHTML="";
-    document.getElementById("errorEtternavn").innerHTML="";
-    document.getElementById("errorTelefonnr").innerHTML="";
-    document.getElementById("errorEpost").innerHTML="";
+
+    $("#film").html("");
+    $("#antallBilleter").html("");
+    $("#fornavn").html("");
+    $("#etternavn").html("");
+    $("#telefonnr").html("");
+    $("#epost").html("");
+
+    $("#errorAntall").html("");
+    $("#errorFilm").html("");
+    $("#errorFornavn").html("");
+    $("#errorEtternavn").html("");
+    $("#errorTelefonnr").html("");
+    $("#errorEpost").html("");
 }
 
-function visBilleter(){
+function hentBilletter(){
+    $.get("/GetTickets", function (data){
+        formaterData(data)
+    })
+}
+
+function formaterData(billeter){
     let ut = "<table><tr><th>Film</th><th>Antall Billeter</th><th>Navn</th><th>Telefonnr</th><th>Epost</th></tr>";
-    for (let person of kinobilleter){
+    for (let billet of billeter){
         ut += "<tr>" +
-            "<td>" + person.film + "</td>" +
-            "<td>" + person.antall + "</td>" +
-            "<td>" + person.fornavn + " " + person.etternavn + "</td>" +
-            "<td>" + person.telefonnr + "</td>" +
-            "<td>" + person.epost + "</td>" +
+            "<td>" + billet.film + "</td>" +
+            "<td>" + billet.antall + "</td>" +
+            "<td>" + billet.fornavn + " " + billet.etternavn + "</td>" +
+            "<td>" + billet.telefonNr + "</td>" +
+            "<td>" + billet.epost + "</td>" +
             "</tr>";
     }
     ut += "</table>";
-    document.getElementById("output").innerHTML=ut;
+    $("#output").html(ut);
 }
 
 function slettKinobillettListe(){
-    kinobilleter=[];
-    document.getElementById("output").innerHTML="";
+    $.get("/DeleteAll", function (){
+        hentBilletter()
+    })
 
 }
